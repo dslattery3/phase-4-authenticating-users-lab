275e527ec8afabf7018f532843354453ae68eb11
Dalton <slattery.da@gmail.com>
2022-03-21 14:12:17 -0500

    commit tree a500bff44e72d078a4093e7238412c87144eb055

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
 
+  private
+
+  def record_not_found(error)
+    render json: { error: "#{error.model} not found" }, status: :not_found
+  end
 end
